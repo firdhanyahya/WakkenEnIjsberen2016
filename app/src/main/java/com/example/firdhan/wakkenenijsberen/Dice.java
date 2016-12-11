73d1f6e5412f13c7c8a08706ec366e8c85f03358
ROBIN T <robin@bash.com>
2016-12-11 13:15:31 +0100

    Latest push

--- app/src/main/java/com/example/firdhan/wakkenenijsberen/Dice.java
+++ app/src/main/java/com/example/firdhan/wakkenenijsberen/Dice.java
@@ -1,5 +1,6 @@
 package com.example.firdhan.wakkenenijsberen;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -8,6 +9,7 @@ import java.util.Random;
 
 public class Dice {
     private int[] dices;
+    private Random r;
 
     public Dice(int aantalDubbelstenen){
         dices = new int[aantalDubbelstenen];
@@ -19,6 +21,14 @@ public class Dice {
         for(int i = 0; i < dices.length; i++){
             dices[i] = randomDices.nextInt(6) + 1;
         }
+    }
+
+    public ArrayList<Integer> Roll(int amount){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < amount; i++){
+            list.add(r.nextInt(6 - 1 + 1) + 1);
+        }
+        return list;
     }
 
     public int[] getDices(){
